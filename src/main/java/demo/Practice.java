4387a0a90557252dab4e4b21161492f388585380
declan4050 <ken4050@gmail.com>
2021-07-22 22:50:41 +0800

    Initial commit 2

--- src/main/java/demo/Practice.java
+++ src/main/java/demo/Practice.java
@@ -4,11 +4,15 @@
     {
         public static void main(String[] args) {
 
-    Bmi result = new Bmi();
+    Bmi result = new Bmi("ken",1.7F,57F);
 
-    result.Weight= 57f;
-    result.Height= 1.7f;
 
+
+
+
+   /* result.Weight= 57f;
+    result.Height= 1.7f;*/
+            System.out.println(result.name);
             System.out.println(result.bmi());
 
 
--- src/main/kotlin/demo/Practice.kt
+++ src/main/kotlin/demo/Practice.kt
@@ -2,15 +2,19 @@ package demo
 
 fun main() {
 
-    val b = BMI1(57f,1.7f)
-
-
+   val b = BMI1()
+    b.Height = 1.7f
+    b.Weight = 57f
     println(b.bmi())
 }
 
 
-class BMI1( var Weight : Float,var Height : Float){
-fun bmi(): Float{
+class BMI1{
+
+    var Weight : Float = 0.0f
+    var Height : Float = 0.0f
+
+    fun bmi(): Float{
 
     val bmi = Weight/(Height*Height)
 
